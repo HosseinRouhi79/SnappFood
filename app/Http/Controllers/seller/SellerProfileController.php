f49bd2cb34222ad8108767ca136aa629bf414eaf
Hossein Rouhi <rouhi1379@gmail.com>
2022-11-08 15:13:35 +0330

    fix some bugs

--- app/Http/Controllers/seller/SellerProfileController.php
+++ app/Http/Controllers/seller/SellerProfileController.php
@@ -83,10 +83,7 @@ class SellerProfileController extends Controller
      * @param int $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        //
-    }
+
 
     /**
      * Show the form for editing the specified resource.
